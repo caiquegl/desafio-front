ea004d8f7f4d91e3a18bfad895712cb0b6d1375d
caiquegl <arq.caique@hotmail.com>
2020-04-18 13:57:52 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var clicks = 0;
+    function mais() {
+        clicks += 1;
+        document.getElementById("clicks").value = clicks;
+    };
+
+    function menos() {
+        clicks -= 1;
+        document.getElementById("clicks").value = clicks;
+    };
+
+
+
